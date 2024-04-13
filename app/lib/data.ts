cd7dcf42948635316a015530c3e1edc21d608666
Kirichenko Nikita <kirichenko.nikita@gmail.com>
2024-04-13 19:33:16 +0300

    запросы и ревенью

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,3 +1,4 @@
+// функция библиотеки @vercel/postgres, позволяющая совершать SQL запросы через Postgres. В этом файле содержатся типовые запросы для работы учебного приложения.  
 import { sql } from '@vercel/postgres';
 import {
   CustomerField,
@@ -9,7 +10,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-
+// посредством запроса возвращает некое свойство строки объекта data, вощвращаемого при запросе `SELECT * FROM revenue`
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
@@ -20,7 +21,7 @@ export async function fetchRevenue() {
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    // Как понимаю, <>указываемое после метода обозначет уточнение типа возвращаемого объекта - дженерик
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
